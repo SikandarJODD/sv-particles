c33af7b9dac8827683ac3e6dc244f4a6a9747bf7
SikandarJODD <sikandar6174@gmail.com>
2026-06-14 00:14:45 +0530

    feat: light and dark mode button for desktop

--- src/lib/components/ui/theme-switch/theme-switch.svelte
+++ src/lib/components/ui/theme-switch/theme-switch.svelte
@@ -3,6 +3,10 @@
 		variant?: "outline" | "ghost";
 		size?: ButtonSize;
 	};
+
+	import { MediaQuery } from "svelte/reactivity";
+
+	let large = new MediaQuery("min-width: 800px");
 </script>
 
 <script lang="ts">
@@ -14,7 +18,7 @@
 	let { variant = "outline", size = "default" }: LightSwitchProps = $props();
 </script>
 
-<Button onclick={toggleMode} {variant} size="icon-sm" class='shadow-none'>
+<Button onclick={toggleMode} {variant} size={large.current ? "icon" : "icon-sm"} class="shadow-none">
 	<SunIcon class="scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90" />
 	<MoonIcon class="absolute scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0" />
 	<span class="sr-only">Toggle theme</span>
