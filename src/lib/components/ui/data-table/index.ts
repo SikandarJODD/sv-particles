ab0af2f83d5c3bb34b399af0ce259670dccc3852
SikandarJODD <sikandar6174@gmail.com>
2026-07-05 21:12:39 +0530

    feat: new data-table component

--- src/lib/components/ui/data-table/index.ts
+++ src/lib/components/ui/data-table/index.ts
@@ -1,4 +1,7 @@
 import TableSearch from './table-search.svelte';
+import TablePagination from './table-pagination.svelte';
+import RowsPerPage from './rows-per-page.svelte';
 import ThSort from './th-sort.svelte';
+import RowCount from './row-count.svelte';
 
-export { TableSearch, ThSort };
+export { TableSearch, ThSort, TablePagination, RowsPerPage, RowCount };
--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import BasicTable from "$lib/components/particles/data-table/basic-table.svelte";
+	import PaginationTable from "$particles/data-table/pagination-table.svelte";
 	import SearchTable from "$particles/data-table/search-table.svelte";
 	import SimpleTable from "$particles/data-table/simple-table.svelte";
 	import SortableTable from "$particles/data-table/sortable-table.svelte";
 </script>
 
-<div class="flex h-screen items-center justify-center">
-	<div class="w-full max-w-5xl space-y-6">
+<div class="flex min-h-screen items-center justify-center">
+	<div class="w-5xl space-y-6">
+		<PaginationTable />
 		<BasicTable />
 		<SimpleTable />
 		<SortableTable />
